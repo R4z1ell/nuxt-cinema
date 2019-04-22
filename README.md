5e0480cffba37ac109a3e5548b14c2695e378222
R4z1ell <creawshaka@gmail.com>
2019-04-22 18:03:02 +0200

    added .env file and updated README.md

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
-# vue-cinema
+# Nuxt-cinema
 
-> My bedazzling Nuxt.js project
+> Movies Web Application powered by Nuxt.js and 'The Movie Database' API.
 
-## Build Setup
+## Setup
 
-``` bash
+```bash
 # install dependencies
 $ yarn install
 
 # serve with hot reload at localhost:3000
 $ yarn run dev
-
-# build for production and launch server
-$ yarn run build
-$ yarn start
-
-# generate static project
-$ yarn run generate
 ```
-
-For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,66 @@
-const pkg = require("./package");
+const pkg = require('./package');
+require('dotenv').config();
 
 module.exports = {
-  mode: "universal",
+  mode: 'universal',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Lato"
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Lato'
       },
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: "#FFFFFF" },
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#FFFFFF' },
 
   /*
-  ** Global CSS
-  */
-  css: ["normalize.css/normalize.css", "~assets/scss/base.scss"],
+   ** Global CSS
+   */
+  css: ['normalize.css/normalize.css', '~assets/scss/base.scss'],
 
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    "~plugins/genre-filter.js",
-    "~plugins/runtime-filter.js",
-    "~plugins/vue-js-modal.js",
-    "~plugins/rating-filter.js"
+    '~plugins/genre-filter.js',
+    '~plugins/runtime-filter.js',
+    '~plugins/vue-js-modal.js',
+    '~plugins/rating-filter.js'
   ],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [],
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/dotenv'],
 
   env: {
-    apiKey: "e280a15596646f3be8074283fd37f3c8"
+    apiKey: process.env.API_KEY
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     postcss: [],
-    vendor: ["vue-js-modal"],
+    vendor: ['vue-js-modal'],
     extend(config, ctx) {}
   }
 };
